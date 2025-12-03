58512a01c5fefb1300895842e90a0739b21695cf
maxBRT <bourret74@gmail.com>
2025-12-03 18:30:15 -0500

    better auth setup

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
+import { auth } from "./lib/auth";
+import { toNodeHandler } from "better-auth/node";
 
 // Load environment variables from .env file
 dotenv.config();
 
 // Create an Express app
 const app = express();
-
-// Add json middleware
+// Add middlewares
+app.all('/api/auth/{*any}', toNodeHandler(auth));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.get("/", (_req, res) => {
+    res.json({ message: "API is running" });
 });
 
 // Start the server
